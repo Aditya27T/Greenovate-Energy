126a73a4983039fdb6e161f4ebe4e867338f4603
Aditya Eka Rahmadani <70711799+Aditya27T@users.noreply.github.com>
2025-06-01 12:03:45 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -14,6 +14,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'unpkg.com',
+        port: '',
+        pathname: '/leaflet@1.9.4/dist/images/**',
+      },
     ],
     // Enable dangerouslyAllowSVG for SVG support if needed
     dangerouslyAllowSVG: true,
